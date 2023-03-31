f041d62a2e67c5867f4290fccbb8d5d3a64b4f50
anijanyan <amirjanyanani@gmail.com>
2023-03-31 16:24:15 +0400

    set tab title

--- src/widgets/tabs/tab.ts
+++ src/widgets/tabs/tab.ts
@@ -87,6 +87,11 @@ export class Tab<SessionType extends EditSession = EditSession> extends TabPanel
         return this.element;
     }
 
+    setTitle(title: string) {
+        this.title = title;
+        this.element.getElementsByClassName("tabTitle")[0].innerHTML = title;
+    }
+
     get isActive(): boolean {
         return this.parent?.activeTab == this;
     }
